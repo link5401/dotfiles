ef23de309ca3d2c31e4bac07b1429300197c361d
link5401 <sieunganinh880@gmail.com>
2021-12-21 16:58:41 +0700

    init

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,14 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt Paradox
+
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'E:\Git\usr\bin\tig.exe'
+Set-Alias less 'E:\Git\usr\bin\less.exe'
+
+
